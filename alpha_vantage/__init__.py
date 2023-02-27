11bdb0a59e0abd6e7a5c6cb220ee17e6402185d8
connordavel <coda3831@colorado.edu>
2023-02-27 02:25:23 -0700

    added initial functionality for AlphaIntelligence and some import info to __init__.py

---
+++ alpha_vantage/__init__.py
@@ -0,0 +1,24 @@
+from alpha_vantage.alphavantage import (
+    AlphaVantage
+)
+from alpha_vantage.cryptocurrencies import (
+    CryptoCurrencies
+)
+from alpha_vantage.foreignexchange import (
+    ForeignExchange
+)
+from alpha_vantage.fundamentaldata import (
+    FundamentalData
+)
+from alpha_vantage.sectorperformance import (
+    SectorPerformances
+)
+from alpha_vantage.techindicators import (
+    TechIndicators
+)
+from alpha_vantage.timeseries import (
+    TimeSeries
+)
+from alpha_vantage.alphaintelligence import (
+    AlphaIntelligence
+)
---
+++ alpha_vantage/alphaintelligence.py
@@ -0,0 +1,27 @@
+from .alphavantage import AlphaVantage as av
+
+
+class AlphaIntelligence(av):
+
+    """This class implements all the api calls to alphaIntelligence
+    """
+    @av._output_format
+    @av._call_api_on_func
+    def get_news(self, symbol, **kwargs):
+        """ Return intraday time series in two json objects as data and
+        meta_data. It raises ValueError when problems arise
+
+        Keyword Arguments:
+            symbol:  the symbol for the equity we want to get its data
+            interval:  time interval between two conscutive values,
+                supported values are '1min', '5min', '15min', '30min', '60min'
+                (default '15min')
+            outputsize:  The size of the call, supported values are
+                'compact' and 'full; the first returns the last 100 points in the
+                data series, and 'full' returns the full-length intraday times
+                series, commonly above 1MB (default 'compact')
+        """
+        _FUNCTION_KEY = "NEWS_SENTIMENT"
+        for key, value in kwargs.items():
+            _FUNCTION_KEY += f"&{key}={value}"
+        return _FUNCTION_KEY, "feed", 'sentiment_score_definition'
